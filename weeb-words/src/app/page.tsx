18a57e4dbeb936b57120be1d9c32e433f06910fc
Taig4 <xTaig4@gmail.com>
2025-03-07 09:25:23 +0100

    Add quotedisplay page

--- weeb-words/src/app/page.tsx
+++ weeb-words/src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import Sidebar from "./Components/Layout/Sidebar";
+import QuoteDisplay from "./Components/Pages/QuoteDisplay";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex min-h-screen">
       <Sidebar />
+      <div className="flex-grow pb-20 ">
+        <QuoteDisplay />
+      </div>
     </div>
   );
 }
