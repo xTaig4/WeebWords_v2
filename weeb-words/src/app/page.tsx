00ef992390c93590079b8d8d8aaceb628494655b
Taig4 <xTaig4@gmail.com>
2025-03-08 16:08:05 +0100

    removed some unused stuff, added prettier plugin, small adjustments

--- weeb-words/src/app/page.tsx
+++ weeb-words/src/app/page.tsx
@@ -1,13 +1,9 @@
-import Image from "next/image";
-import Sidebar from "./Components/Layout/Sidebar";
-import QuoteDisplay from "./Components/Pages/QuoteDisplay";
-
 export default function Home() {
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
-      <div className="flex-grow pb-20 ">
-        <QuoteDisplay />
+      
+      <div className="flex-grow">
+        
       </div>
     </div>
   );
